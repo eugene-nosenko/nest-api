1207987802a9329cde6fdd579202d52a57381761
Eugene Nosenko <eugene.n@code-care.pro>
2021-12-28 23:38:54 +0200

    changed default paths and nodemon

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TagService {
   findAll(): string[] {
-    return ['dragons', 'coffee', 'cats'];
+    return ['dragons', 'coffee', 'cats', 'dogs'];
   }
 }
