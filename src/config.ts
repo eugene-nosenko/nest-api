1e91e9290f31a7c4999a45b69259e830169dd9f2
Eugene Nosenko <eugene.n@code-care.pro>
2022-01-04 00:21:51 +0200

    Added JWT token

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const JWT_SECRET = 'super-secret-random-code-988u8utgk';
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,10 @@
+import { JWT_SECRET } from '@app/config';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { sign } from 'jsonwebtoken';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
 import { UserEntity } from './user.entity';
 
 @Injectable()
@@ -16,5 +19,25 @@ export class UserService {
     Object.assign(newUser, createUserDto);
     console.log('newUser', newUser);
     return this.userRepository.save(newUser);
+  }
+
+  generateJWT(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      JWT_SECRET,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJWT(user),
+      },
+    };
   }
 }
