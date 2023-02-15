7848b4d864ccdb1cdc8d68ab3ac59c004e9214f6
finn <eugene.n@code-care.pro>
2023-02-15 20:55:18 -0300

    added follow api

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,5 +1,6 @@
 import { User } from '@app/user/decorators/user.decorator';
-import { Controller, Get, Param } from '@nestjs/common';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ProfileService } from './profile.sevice';
 import { ProfileResponseInterface } from './types/profileResponse.types';
 
@@ -12,6 +13,19 @@ export class ProfileController {
     @Param('username') profileUsername: string,
   ): Promise<ProfileResponseInterface> {
     const profile = await this.profileService.getProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followUser(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.followProfile(
       currentUserId,
       profileUsername,
     );
--- src/profile/profile.sevice.ts
+++ src/profile/profile.sevice.ts
@@ -2,6 +2,7 @@ import { UserEntity } from '@app/user/user.entity';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { FollowEntity } from './follow.entity';
 import { ProfileType } from './types/profile.types';
 import { ProfileResponseInterface } from './types/profileResponse.types';
 
@@ -10,6 +11,9 @@ export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userReposytory: Repository<UserEntity>,
+
+    @InjectRepository(FollowEntity)
+    private readonly followReposytory: Repository<FollowEntity>,
   ) {}
   async getProfile(
     currentUserId: number,
@@ -24,6 +28,40 @@ export class ProfileService {
     }
 
     return { ...user, following: false };
+  }
+
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userReposytory.findOne({
+      username: profileUsername,
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'You cannot follow yourself',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followReposytory.findOne({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+      await this.followReposytory.save(followToCreate);
+    }
+
+    return { ...user, following: true };
   }
 
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
